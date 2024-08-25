39d25d3bcc0ef60ac8507937a7b8a7b5b19a5c27
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-08-25 15:11:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,12 +9,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 import blogRouter from "./routes/blogRoute.js";
 import userRouter from "./routes/userRoute.js";
-import { fileURLToPath } from "url";
+
 app.use(blogRouter);
 app.use(userRouter);
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+
 
 export { app };
